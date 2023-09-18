82f240e8ef391fcab9f66ebdcbe475e0274d2783
Nishanth <rsbweb123@gmail.com>
2023-09-18 17:52:08 +0530

    Updated

--- src/main/java/com/rsbweb/repeaterweb/RepeaterWebApplication.java
+++ src/main/java/com/rsbweb/repeaterweb/RepeaterWebApplication.java
@@ -17,7 +17,7 @@ public class RepeaterWebApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+				registry.addMapping("/**");
 			}
 		};
 	}
